14a5372d36ad130dcf74bb5072d73e0b885e75c1
Nylzen <tugoresn@gmail.com>
2024-05-24 17:24:33 -0300

    add: oil nvim

--- lua/nylzen/core/keymaps.lua
+++ lua/nylzen/core/keymaps.lua
@@ -33,20 +33,23 @@ keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Lazy load" })
 
 -- Save file
 keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-keymap.set({"i","x","n","s"}, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- Move lines
 keymap.set("n", "<A-j>", "<cmd>m .+1<CR>==", { desc = "Move line down" })
 keymap.set("n", "<A-k>", "<cmd>m .-2<CR>==", { desc = "Move line up" })
 keymap.set("i", "<A-j>", "<esc><cmd>m .+1<CR>==gi", { desc = "Move line down" })
 keymap.set("i", "<A-k>", "<esc><cmd>m .+1<CR>==gi", { desc = "Move line up" })
-keymap.set("v", "<A-j>",":m '>+1<cr>gv=gv", { desc = "Move line down" })
-keymap.set("v", "<A-k>",":m '<-2<cr>gv=gv", { desc = "Move line up" })
+keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
+keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
 
 -- better indenting
 keymap.set("v", "<", "<gv", { desc = "Indent left" })
 keymap.set("v", ">", ">gv", { desc = "Indent right" })
 
 -- codeshot
-keymap.set('v', "<leader>sc", ":SSSelected<CR>", { desc = "Capture selected code" })
-keymap.set('v', "<leader>sh", ":SSFocused<CR>", { desc = "Capture current file" })
+keymap.set("v", "<leader>sc", ":SSSelected<CR>", { desc = "Capture selected code" })
+keymap.set("v", "<leader>sh", ":SSFocused<CR>", { desc = "Capture current file" })
+
+-- oil
+vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
